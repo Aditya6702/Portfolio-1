8e399b527a633f1c20ad714f6938f114adbbe38b
Aditya6702 <adityasailella@gmail.com>
2024-06-09 00:10:59 +0530

    portfolio commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,49 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  // {
+  //   label: 'Articles',
+  //   pathname: '/articles',
+  // },
+  {
+    label: 'Contact',
+    pathname: '/contact1',
+  },
+  // {
+  //   label: 'Photography',
+  //   pathname: 'https://photography.tomasps.com',
+  // },
+  // {
+  //   label: 'Drive',
+  //   pathname: 'https://drive.tomasps.com',
+  // },
+];
+
+export const socialLinks = [
+  // {
+  //   label: 'Twitter',
+  //   url: 'https://twitter.com/toomas_ps',
+  //   icon: 'twitter',
+  // },
+  {
+    label: 'Github',
+    url: 'https://github.com/Aditya6702',
+    icon: 'github',
+  },
+  {
+    label:'Linkedin',
+    url: 'https://www.linkedin.com/in/aditya-sai-lella-800b8927a/',
+    icon: 'linkedin'
+  },
+  // {
+  //   label:'codechef',
+  //   url:'https://www.codechef.com/users/adityasailella',
+  //   icon:'codechef'
+  // }
+];
